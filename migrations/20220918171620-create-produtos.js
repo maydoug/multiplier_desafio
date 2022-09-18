0fc888fe4ebf4956106a6b0162375f0295fa0ec3
unknown <maydoug@hotmail.com>
2022-09-18 14:47:07 -0300

    feat(banco_de_dados): migration de produtos

---
+++ migrations/20220918171620-create-produtos.js
@@ -0,0 +1,52 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+   
+    await queryInterface.createTable('produtos', { 
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      idCategoria: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'categorias',
+          key: 'id'
+        }
+      },
+      codigo: {
+        type: Sequelize.STRING(50),
+        allowNull: false,
+      },
+      nome: {
+        type: Sequelize.STRING(100),
+        allowNull: false,
+      },
+      descricao: {
+        type: Sequelize.TEXT('medium'),
+        allowNull: false,
+      },
+      valor: {
+        type: Sequelize.DECIMAL(11, 10),
+        allowNull: false,
+      },
+      status: {
+        type: Sequelize.TINYINT(1),
+        allowNull: false,
+      },
+    
+    });
+    
+
+  },
+
+  async down (queryInterface, Sequelize) {
+ 
+    await queryInterface.dropTable('produtos');
+   
+  }
+};
