1bc4053217d4354734037944e85ca7fb72a45e0d
unknown <maydoug@hotmail.com>
2022-09-18 15:05:50 -0300

    feat(banco_de_dados): migration de estoque

---
+++ migrations/20220918174909-create-estoque.js
@@ -0,0 +1,44 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+  
+    await queryInterface.createTable('estoque', { 
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      idProduto: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'produtos',
+          key: 'id'
+        }
+      },
+      quantidade: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      reserva: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      status: {
+        type: Sequelize.TINYINT(1),
+        allowNull: false,
+      },
+    
+    });
+
+  },
+
+  async down (queryInterface, Sequelize) {
+  
+    
+     await queryInterface.dropTable('estoque');
+    
+  }
+};
